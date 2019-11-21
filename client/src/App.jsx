b5009e02f15fd3f65e27eca845258f1ceddf067d
Patrick Wong <pwong288@gmail.com>
2019-11-21 11:02:35 -0800

    implement test graph using graph.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import BPIChart from './Chart';
 
 function App() {
+  // const [bpi]
   return (
-    <div>HELLOWORLD</div>
+    <div>
+      <h1>Cryptocurrency Charting Tool</h1>
+      <BPIChart />
+      <p>Powered By CoinDesk</p>
+    </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.resolve(__dirname, '../client/public/')));
-// app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/api/bpiData', (req, res) => {
+  
+});
 
 app.listen(port, () => console.log(`Crypto Chart listening on port ${port}!`));
+
+// https://api.coindesk.com/v1/bpi/currentprice.json
+// https://api.coindesk.com/v1/bpi/historical/close.json
